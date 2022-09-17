75154444b1598bedc75cc5fb2ea3679dd2d479ec
RodrigoVerasMariae <112950516+RodrigoVerasMariae@users.noreply.github.com>
2022-09-17 16:20:20 -0300

    Adicionando IF/ELSE

--- codigos.py/ex25.py
+++ codigos.py/ex25.py
@@ -1,3 +1,12 @@
-nome = str(input('Qual seu nome?')).strip()
-print('No seu nome pussui Veras? {}'.format('veras' in nome.lower()))
+''''nome = str(input('Qual seu nome?')).strip()
+print('No seu nome pussui Veras? {}'.format('veras' in nome.lower()))'''
 
+# Exemplo utilizando media
+n1 = int(input('Nota da prova: '))
+n2 = int(input('Nota do trabalho? '))
+media = (n1+n1)/2
+print('Sua media foi {}'.format(media))
+if media >= 5:
+    print('Ufa, passou em')
+else:
+    print('Tenha vergonha, vá estudar')
