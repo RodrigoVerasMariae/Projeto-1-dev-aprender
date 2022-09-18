742672160daabf1dee6796a4df55623b910b8144
RodrigoVerasMariae <112950516+RodrigoVerasMariae@users.noreply.github.com>
2022-09-17 22:26:41 -0300

    Novo desafio, esse é o 29

---
+++ codigos.py/desafio29.py
@@ -0,0 +1,8 @@
+velocidade = float(input('Qual a velodiade do carro? '))
+if velocidade > 80:
+    multa = (velocidade - 80) * 7
+    print('MULTADO PELA A LEI DE TRANSITO!!! Valor de sua multa {:.2f}'.format(multa))
+
+print('='*20)
+print('Dirija com cuidado!')
+print('='*20)
