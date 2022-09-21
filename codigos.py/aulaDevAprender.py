8caad506d5170f8ede6e5355f3b91ea0a2f05e93
RodrigoVerasMariae <112950516+RodrigoVerasMariae@users.noreply.github.com>
2022-09-21 10:01:30 -0300

    Mais um exemplo sobre condição no exercicio do Dev Aprender

---
+++ codigos.py/aulaDevAprender.py
@@ -0,0 +1,29 @@
+'''salario = float(input('Qual seu salario: '))
+Horas = float(input('Qual seu tempo de trabalho: '))
+valor_hora = salario/Horas
+print('Seu salario por hora é igual a:R${:.2f}'.format(valor_hora))'''
+
+'''ajudar = False
+if ajudar == True:
+    print('Se eu tiver livre, ajudo sim!')
+else:
+    print('Meio ocupado, veja com outra pessoa!')'''
+
+'''atrasado = int(input('Quantas vezes voce chegou atrasado: '))
+if atrasado == 3:
+    print('É a ultima em!')
+if atrasado < 3:
+    print('Pode entrar!')
+if atrasado > 3:
+    print('Caia fora!')'''
+
+    valor1 = float(input('Digita o primeiro valor: '))
+valor2 = float(input('Digita valor 2: '))
+if valor1 > valor2:
+    print('{:.2f} é maior {:.2f}'.format(valor1,valor2))
+
+if valor1 == valor2:
+    print('Os valores são iguais')
+if valor2 > valor1:
+    print('{:.2f} é maior que {:.2f}.'.format(valor2, valor1))
+    
